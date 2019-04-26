b1815188c45d2993bbcd59fc4d2235ef48dee723
Dwight Watson <dwightwatson@me.com>
2019-04-26 18:35:09 +1000

    Adjust prismjs ignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         tailwind: true,
-        ignore: ['prismjs']
+        ignore: ['prismjs/']
       }
     },
     {
