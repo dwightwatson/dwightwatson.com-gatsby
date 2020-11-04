d4b8ffbfb2feedad463ae66ac30970852482b3e8
Dwight Watson <dwightwatson@users.noreply.github.com>
2020-11-04 15:08:36 +1100

    Improve date formatting

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -11,7 +11,7 @@ class Index extends React.Component {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
-    const { currentPage, numberOfPages } = this.props.pageContext;
+    const { currentPage, numberOfPages } = this.props.pageContext
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -21,19 +21,31 @@ class Index extends React.Component {
         />
         <Bio />
 
-        {posts.map(({ node }) => <Post key={node.id} node={node} />)}
+        {posts.map(({ node }) => (
+          <Post key={node.id} node={node} />
+        ))}
 
         <ul className="mb-5">
           <li className="mb-5">
             {currentPage > 1 && (
-              <Link to={`/pages/${currentPage - 1}`} rel="prev" className="underline">
+              <Link
+                to={`/pages/${currentPage - 1}`}
+                rel="prev"
+                className="underline"
+              >
                 ← Previous page
               </Link>
             )}
           </li>
           <li className="text-right">
             {currentPage < numberOfPages && (
-              <Link to={`/pages/${currentPage + 1}`} rel="next" className="underline">Next page  →</Link>
+              <Link
+                to={`/pages/${currentPage + 1}`}
+                rel="next"
+                className="underline"
+              >
+                Next page →
+              </Link>
             )}
           </li>
         </ul>
@@ -63,7 +75,7 @@ export const pageQuery = graphql`
           frontmatter {
             title
             path
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "MMMM D, YYYY")
           }
         }
       }
