9cf39567956c81a6a4141c2c27dfde9ad8f50b07
Dwight Watson <dwightwatson@users.noreply.github.com>
2020-11-25 11:12:24 +1100

    Move post markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown`,
-        path: `${__dirname}/src/markdown`,
+        path: `${__dirname}/src/posts`,
       },
     },
     {
