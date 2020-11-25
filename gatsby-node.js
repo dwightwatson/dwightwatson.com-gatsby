ee7d8f257e09127ee1021969d565816c33c419cb
Dwight Watson <dwightwatson@users.noreply.github.com>
2020-11-25 11:43:18 +1100

    Use async

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,14 @@
 const _ = require(`lodash`)
 const path = require(`path`)
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const postsTemplate = path.resolve(`./src/templates/posts.js`)
   const postTemplate = path.resolve(`./src/templates/post.js`)
   const tagsTemplate = path.resolve("src/templates/tags.js")
 
-  return graphql(
+  const result = await graphql(
     `
       {
         allMarkdownRemark(
@@ -28,65 +28,58 @@ exports.createPages = ({ graphql, actions }) => {
         }
       }
     `
-  ).then((result) => {
-    if (result.errors) {
-      throw result.errors
+  )
+
+  const posts = result.data.allMarkdownRemark.edges
+
+  const postsPerPage = 10
+  const numberOfPages = Math.ceil(posts.length / postsPerPage)
+
+  Array.from({ length: numberOfPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/` : `/pages/${i + 1}`,
+      component: postsTemplate,
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numberOfPages,
+        currentPage: i + 1,
+      },
+    })
+  })
+
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: post.node.frontmatter.path,
+      component: postTemplate,
+      context: {
+        previous,
+        next,
+      },
+    })
+  })
+
+  let tags = []
+
+  _.each(posts, (edge) => {
+    if (_.get(edge, "node.frontmatter.tags")) {
+      tags = tags.concat(edge.node.frontmatter.tags)
     }
+  })
 
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
+  tags = _.uniq(tags)
 
-    const postsPerPage = 10
-    const numberOfPages = Math.ceil(posts.length / postsPerPage)
-
-    Array.from({ length: numberOfPages }).forEach((_, i) => {
-      createPage({
-        path: i === 0 ? `/` : `/pages/${i + 1}`,
-        component: postsTemplate,
-        context: {
-          limit: postsPerPage,
-          skip: i * postsPerPage,
-          numberOfPages,
-          currentPage: i + 1,
-        },
-      })
+  tags.forEach((tag) => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag)}/`,
+      component: tagsTemplate,
+      context: {
+        tag,
+      },
     })
-
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-
-      createPage({
-        path: post.node.frontmatter.path,
-        component: postTemplate,
-        context: {
-          previous,
-          next,
-        },
-      })
-    })
-
-    let tags = []
-
-    _.each(posts, (edge) => {
-      if (_.get(edge, "node.frontmatter.tags")) {
-        tags = tags.concat(edge.node.frontmatter.tags)
-      }
-    })
-
-    tags = _.uniq(tags)
-
-    tags.forEach((tag) => {
-      createPage({
-        path: `/tags/${_.kebabCase(tag)}/`,
-        component: tagsTemplate,
-        context: {
-          tag,
-        },
-      })
-    })
-
-    return null
   })
 }
 
