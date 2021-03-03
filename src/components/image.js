c3f98aaae6ed1b01a3656f6d613b649ca0ba0eac
Dwight Watson <dwightwatson@users.noreply.github.com>
2021-03-04 09:49:22 +1100

    Upgrade to Gatsby 3

--- src/components/image.js
+++ src/components/image.js
@@ -1,32 +1,25 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
-
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `StaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `StaticQuery`: https://gatsby.dev/staticquery
- */
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const Image = () => (
   <StaticQuery
     query={graphql`
-      query {
+      {
         placeholderImage: file(relativePath: { eq: "dwight-watson.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 80) {
-              ...GatsbyImageSharpFluid
-            }
+            gatsbyImageData(width: 80, layout: CONSTRAINED)
           }
         }
       }
     `}
-    render={data => <div className="inline-block rounded-full overflow-hidden w-20"><Img fluid={data.placeholderImage.childImageSharp.fluid} /></div>}
+    render={(data) => (
+      <GatsbyImage
+        className="inline-block rounded-full overflow-hidden w-20"
+        alt=""
+        image={data.placeholderImage.childImageSharp.gatsbyImageData}
+      />
+    )}
   />
 )
 
